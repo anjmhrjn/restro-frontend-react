a4f262801f9dfa2464215e3d8dad01979f978540
anjmhrjn <anjmhrjn1@gmail.com>
2022-02-02 11:35:29 +0545

    Designed my tables ui

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,43 @@
+import { useContext } from "react";
+import { UserTypeContext } from "../App";
+
 const Dashboard = () => {
+    const {state2, dispatch2} = useContext(UserTypeContext);
+
+    const RenderBusinessContent = () => {
+        return (
+            <>
+                This is business content
+                <br/>
+            </>
+        )
+    }
+
+    const RenderCustomerContent = () => {
+        return (
+            <>
+                This is customer content
+                <br/>
+            </>
+        )
+    }  
+
+    let content = '';
+
+    if (state2 === 'Business') {
+        content = RenderBusinessContent()
+    } else if (state2 === 'Customer') {
+        content = RenderCustomerContent()
+    }
+    
+
+
+
     const username = localStorage.getItem('username')
 
     return (
         <div className="container mt-5 pt-5">
+            {content}
             This is dashboard. Welcome user: {username}
         </div>
     )
--- src/components/business/MyTables.js
+++ src/components/business/MyTables.js
@@ -5,15 +5,72 @@ const MyTables = () => {
         <div className="mt-5 pt-4">
             <section className="container-fluid bg-secondary">
                 <div className="text-center py-3">
-                    <p className="display-4">My Tables</p>
+                    <p className="fs-1 fw-bold">My Tables</p>
                     <p class="fst-italic">Here are the list of tables that were added by you.</p>
                     <Link to='/table-add'>
-                    <a className="btn btn-secondary" href="#">Add Tables</a>
+                    <a className="btn btn-primary" href="#">Add Tables</a>
                     </Link>
-                    
-
                 </div>
             </section>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12 col-lg-9 mx-auto">
+                        <div className="table-responsive">
+                            <table className="table table-striped custom-table text-center">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Table Number</th>
+                                        <th>Minimum Capacity</th>
+                                        <th>Maximum Capacity</th>
+                                        <th>Is Available</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-dark">
+                                    <tr>
+                                        <td>1</td>
+                                        <td>2</td>
+                                        <td>1</td>
+                                        <td>5</td>
+                                        <td>True</td>
+                                        <td>
+                                            <div className="d-flex justify-content-center buttonsList">
+                                            <button className="btn btn-sm btn-outline-primary me-2" data-tooltip="Update"><i class="far fa-edit"></i></button>
+                                            <button className="btn btn-sm btn-outline-danger" data-tooltip="Update"><i class="far fa-trash-alt"></i></button>
+
+                                            </div>
+                                            
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>2</td>
+                                        <td>1</td>
+                                        <td>5</td>
+                                        <td>True</td>
+                                        <td>
+                                            <button className="btn btn-outline-primary">Update</button>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>2</td>
+                                        <td>1</td>
+                                        <td>5</td>
+                                        <td>True</td>
+                                        <td>
+                                            <button className="btn btn-primary">Update</button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     )
 }
